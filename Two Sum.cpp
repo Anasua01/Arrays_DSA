47c60ec5d9cbc00876470ac0181e78ec1fa3c08f
Anasua Saha <106821608+Anasua01@users.noreply.github.com>
2023-06-30 22:19:54 +0530

    Create Two Sum.cpp

---
+++ Two Sum.cpp
@@ -0,0 +1,21 @@
+#include <unordered_map>
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mpp;
+
+        for(int i=0;i<nums.size();i++){
+            if(mpp.find(target-nums[i])==mpp.end()){
+                mpp[nums[i]]=i;
+            }
+            else{
+                return {mpp[target-nums[i]],i};
+            }
+        }
+        return {-1,-1};
+    }
+};
+
+//Time Complexity: O(N)
+//Space Complexity: O(N) 
